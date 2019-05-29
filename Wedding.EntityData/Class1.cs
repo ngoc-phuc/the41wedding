788f2e41e8c9b8302f71e938bb3c7b8de4dbe131
HOAN <thanghoan0309@gmail.com>
2019-05-30 00:14:59 +0700

    init web project

---
+++ Wedding.EntityData/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Wedding.EntityData
+{
+    public class Class1
+    {
+    }
+}
---
+++ Wedding.EntityData/Schemas/Table.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wedding.EntityData.Schemas
+{
+    public class Table : CoreTable
+    {
+        public int? InsertUserCd { get; set; }
+
+        public int? UpdateUserCd { get; set; }
+    }
+
+    public class CoreTable
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Cd { get; set; }
+
+        //[DisplayFormat(DataFormatString = "{0:yyyy年M月d日}", ApplyFormatInEditMode = true)]
+        public DateTime? InsertDate { get; set; }
+
+        //[DisplayFormat(DataFormatString = "{0:yyyy年M月d日}", ApplyFormatInEditMode = true)]
+        public DateTime? UpdateDate { get; set; }
+    }
+
+    public class DeleteFlgTable : Table
+    {
+        public byte DelFlag { get; set; }
+    }
+}
