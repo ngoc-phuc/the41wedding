1deea1a1d6efdf1154216506d150057f36170cad
Hoan Nguyễn Đình <ndhoanit1112@gmail.com>
2019-06-05 23:58:08 +0700

    coding studio products list.

---
+++ Wedding.Web/Controllers/StudioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abstractions.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wedding.Web.Controllers
+{
+    public class StudioController : Controller
+    {
+        private readonly IWeddingStudioService _weddingStudioService;
+
+        public StudioController(IWeddingStudioService weddingStudioService)
+        {
+            _weddingStudioService = weddingStudioService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsList(int studioId)
+        {
+            var listProducts = _weddingStudioService.GetWeddingStudioAsync(studioId);
+            return View(listProducts);
+        }
+    }
+}
